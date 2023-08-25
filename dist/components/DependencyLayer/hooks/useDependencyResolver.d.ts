aff46b25131fedd65f98edbefe5c933737d37356
irishcarbomb777 <danielclannan@gmail.com>
2023-08-25 13:52:00 -0400

    mod

--- dist/components/DependencyLayer/hooks/useDependencyResolver.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const useDependencyResolver: ({ callback, cleanup }: {
-    callback: (setDependencyResolved: (resolved: boolean) => void) => void;
-    cleanup?: (setCleaningUp: (cleaningUp: boolean) => void) => void;
-}) => void;
